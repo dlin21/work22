cf86c6c6d135c40fa2d2bc3264dc45a0057316cb
dlin21 <90690772+dlin21@users.noreply.github.com>
2021-12-17 13:08:42 -0500

    Update basic_client.c

--- basic_client.c
+++ basic_client.c
@@ -1,1 +1,21 @@
+#include "pipe_networking.h"
 
+
+int main() {
+
+  int to_server;
+  int from_server;
+  char buffer[BUFFER_SIZE];
+  
+  from_server = client_handshake(&to_server);
+  while(1){
+    printf("message to server: ");
+    fgets(line, BUFFER_SIZE, stdin);
+    
+    write(to_server, buffer, sizeof(buffer));
+    read(from_server, buffer, sizeof(buffer));
+    printf("server response: %s\n", buffer);
+  }
+
+  return 0;
+}

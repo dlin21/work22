234e093dd9fe0c88d277de84fc64cd4777dbc976
dlin21 <90690772+dlin21@users.noreply.github.com>
2021-12-17 13:12:26 -0500

    Update basic_server.c

--- basic_server.c
+++ basic_server.c
@@ -1,1 +1,24 @@
+#include "pipe_networking.h"
 
+
+int main() {
+
+  int to_client;
+  int from_client;
+  
+  while(1){
+    from_client = server_handshake(&to_client);
+    
+    char buffer[BUFFER_SIZE];
+    while(1){
+      // read data from client
+      if(read(from_client, buffer, sizeof(buffer)) == 0) break;
+      
+      int i;
+      for(i=0; i<strlen(buffer); i++) buffer[i] = tolower(buffer[i]);
+      write(to_client, buffer, sizeof(buffer));
+    }
+  }
+
+  from_client = server_handshake( &to_client );
+}

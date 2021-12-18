f0529682cb75ee1fdb6c8d35706b59111fe187ca
dlin21 <90690772+dlin21@users.noreply.github.com>
2021-12-18 12:36:20 -0500

    Update basic_server.c

--- basic_server.c
+++ basic_server.c
@@ -7,18 +7,22 @@ int main() {
   int from_client;
   
   while(1){
-    from_client = server_handshake(&to_client);
+    from_client = server_setup();
+    int f = fork();
+    if (f){}
+    else{
     
-    char buffer[BUFFER_SIZE];
-    while(1){
-      // read data from client
-      if(read(from_client, buffer, sizeof(buffer)) == 0) break;
+      char buffer[BUFFER_SIZE];
+      to_client = server_connect(from_client);
       
-      int i;
-      for(i=0; i<strlen(buffer); i++) buffer[i] = tolower(buffer[i]);
-      write(to_client, buffer, sizeof(buffer));
+      while(1){
+        // read data from client
+        if(read(from_client, buffer, sizeof(buffer)) == 0) break;
+      
+        int i;
+        for(i=0; i<strlen(buffer); i++) buffer[i] = tolower(buffer[i]);
+        write(to_client, buffer, sizeof(buffer));
     }
+   }
   }
-
-  from_client = server_handshake( &to_client );
 }

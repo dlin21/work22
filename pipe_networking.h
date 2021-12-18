8bbe72642be65778e50bff5741a5193ee1f1ff71
dlin21 <90690772+dlin21@users.noreply.github.com>
2021-12-18 14:12:18 -0500

    Update pipe_networking.h

--- pipe_networking.h
+++ pipe_networking.h
@@ -15,6 +15,9 @@
 #define HANDSHAKE_BUFFER_SIZE 10
 #define BUFFER_SIZE 1000
 
+int server_setup();
+int server_connect(int from_client);
+
 int server_handshake(int *to_client);
 int client_handshake(int *to_server);
 

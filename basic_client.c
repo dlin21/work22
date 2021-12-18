59ba22ec55d1417f6f354727a0711d160d50fee1
dlin21 <90690772+dlin21@users.noreply.github.com>
2021-12-18 14:11:13 -0500

    Update basic_client.c

--- basic_client.c
+++ basic_client.c
@@ -10,7 +10,7 @@ int main() {
   from_server = client_handshake(&to_server);
   while(1){
     printf("message to server: ");
-    fgets(line, BUFFER_SIZE, stdin);
+    fgets(buffer, BUFFER_SIZE, stdin);
     
     write(to_server, buffer, sizeof(buffer));
     read(from_server, buffer, sizeof(buffer));
